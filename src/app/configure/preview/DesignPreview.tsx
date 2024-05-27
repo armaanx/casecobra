2e2a9761cd9eb1b98af7a5227b6a11fd50cb0351
Armaan Mishra <armaanmishra48@gmail.com>
2024-05-27 10:55:22 +0530

    added stripe webhook integration and updated auth

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -12,10 +12,14 @@ import Confetti from "react-dom-confetti";
 import { createCheckoutSession } from "./actions";
 import { useRouter } from "next/navigation";
 import { useToast } from "@/components/ui/use-toast";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import LoginModal from "@/components/LoginModal";
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   useEffect(() => {
     setShowConfetti(true);
   }, []);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+  const { user } = useKindeBrowserClient();
   const router = useRouter();
   const { toast } = useToast();
   const [showConfetti, setShowConfetti] = useState(false);
@@ -51,6 +55,14 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
       });
     },
   });
+  const handleCheckout = () => {
+    if (user) {
+      createPaymentSession({ configId: configuration.id });
+    } else {
+      localStorage.setItem("configurationId", configuration.id);
+      setIsLoginModalOpen(true);
+    }
+  };
   return (
     <>
       <div
@@ -62,6 +74,9 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
           config={{ elementCount: 200, spread: 90 }}
         />
       </div>
+
+      <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} />
+
       <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
         <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
@@ -135,9 +150,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             </div>
             <div className="mt-8 flex justify-end pb-12">
               <Button
-                onClick={() =>
-                  createPaymentSession({ configId: configuration.id })
-                }
+                onClick={() => handleCheckout()}
                 className="px-4 sm:px-6 lg:px-8"
               >
                 Check Out
